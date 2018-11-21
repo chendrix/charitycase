05ba831ff53b55232348c6ed34379cc5d45ca894
Chris Hendrix <chendrix@pivotal.io>
2018-11-21 14:14:39 -0800

    Open the sqlite database to count the number of entries

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/chendrix/charitycase
 
-require github.com/jessevdk/go-flags v1.4.0
+require (
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/mattn/go-sqlite3 v1.10.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"os"
 
 	flags "github.com/jessevdk/go-flags"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Options struct {
-	CharityFilePath      string   `short:"c" description:"path to charity CSV"`
-	GovernmentGrantsPath []string `short:"g" description:"path to a Form 990 Part VIII line 1e CSV from Open990"`
+	CharityFilePath              string `short:"c" description:"path to charity CSV"`
+	GovernmentGrantsDatabasePath string `short:"d" description:"path to a sqlite3 database populated with entries from Form 990 Part VIII line 1e CSV from Open990"`
 }
 
 func main() {
@@ -23,6 +28,23 @@ func main() {
 		}
 	}
 
+	errLog := log.New(os.Stderr, "", 0)
+	database, err := sql.Open("sqlite3", opts.GovernmentGrantsDatabasePath)
+	if err != nil {
+		errLog.Printf("Failed to open sqlite database at path: %s\n%s", opts.GovernmentGrantsDatabasePath, err.Error())
+		os.Exit(1)
+	}
+
+	row := database.QueryRow("SELECT COUNT(*) FROM grants")
+
+	var count int
+	err = row.Scan(&count)
+	if err != nil {
+		errLog.Printf("Failed to read entry count from database:\n%s", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Count: %d\n", count)
 }
 
 //
